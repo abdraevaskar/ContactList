99e89f95636b7a54fefaa0e986225c6a8600d5ca
abdraevaskar <askarabdraev@gmail.com>
2020-05-06 23:33:58 +0600

    my commit

---
+++ ContactList.py
@@ -0,0 +1,12 @@
+class ContactList(list):
+    def search_by_name(self, name):
+        namelist = []
+        for self.name in self:
+            if name in self.name:
+                namelist.append(self.name)
+        return namelist
+                
+all_contacts = ContactList(input('Введите список имен черех запятую: ').split(','))
+print(all_contacts.search_by_name(input('Введите имя для списка: ')))
+
+
